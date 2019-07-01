7e7985e8bf108b72d0c8d8efcfcabb0ad66a9f96
MMangus <mjmangus16@yahoo.com>
2019-07-01 11:48:56 -0400

    building out lyrics page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Provider } from "./context";
 
 import Navbar from "./components/layout/Navbar.js";
 import Index from "./components/layout/Index.js";
+import Lyrics from "./components/tracks/Lyrics";
 
 import "./App.css";
 
@@ -17,6 +18,7 @@ function App() {
           <div className="container">
             <Switch>
               <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
             </Switch>
           </div>
         </>
